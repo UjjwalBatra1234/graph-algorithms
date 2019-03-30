dafa72d7632346335e297498a94f56c4e664ae04
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-03-30 22:30:28 +1100

    add edge and update edge complete and tested

--- IncidenceMatrix.java
+++ IncidenceMatrix.java
@@ -14,16 +14,13 @@ import java.util.List;
 public class IncidenceMatrix extends AbstractAssocGraph {
 
     private HashMap<String, Integer> vertices = new HashMap<>();
-    private HashMap<Edge, Integer> edges = new HashMap<>();
+    private HashMap<String, Integer> edges = new HashMap<>();
     private int[][] incidenceMatrix = new int[this.vertices.size()][this.edges.size()];
 
     /**
      * Contructs empty graph.
      */
     public IncidenceMatrix() {
-        // Implement me
-        //I'll write my code here
-
     } // end of IncidentMatrix()
 
 
@@ -35,25 +32,70 @@ public class IncidenceMatrix extends AbstractAssocGraph {
             this.addRow();
             this.vertices.put(upperCaseVertLabel, this.incidenceMatrix.length - 1);
         }
+        printArray();
     } // end of addVertex()
 
 
     public void addEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
+        Edge newEdge = new Edge(srcLabel, tarLabel);
+
+        //insert edge only if it does not exist and source and target exists
+        if (!this.edges.containsKey(newEdge.key)
+                && this.vertices.containsKey(newEdge.source)
+                && this.vertices.containsKey(newEdge.target)) {
+            this.addColumn();
+            this.edges.put(newEdge.key, this.incidenceMatrix[0].length - 1);
+        }
+        this.addEdgeWeight(newEdge, weight);
+        printArray();
+
     } // end of addEdge()
 
+    //    TODO: remove this
+    private void printArray() {
+        for (int i = 0; i < incidenceMatrix.length; i++) {
+            for (int j = 0; j < incidenceMatrix[0].length; j++) {
+                System.out.print(incidenceMatrix[i][j]);
+            }
+            System.out.printf("\n");
+        }
+
+        System.out.println(vertices);
+        System.out.println(edges);
+
+    }
 
     public int getEdgeWeight(String srcLabel, String tarLabel) {
-        // Implement me!
+        Edge edge = new Edge(srcLabel, tarLabel);
 
-        // update return value
+        if (this.edges.containsKey(edge.key)) {
+            int edgeIndex = this.edges.get(edge.key);
+            int srcIndex = this.vertices.get(edge.source);
+
+            return this.incidenceMatrix[srcIndex][edgeIndex];
+        }
+
         return EDGE_NOT_EXIST;
     } // end of existEdge()
 
 
     public void updateWeightEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
+        Edge edge = new Edge(srcLabel, tarLabel);
+
+        addEdgeWeight(edge, weight);
     } // end of updateWeightEdge()
+
+    private void addEdgeWeight(Edge edge, int weight) {
+        if (this.edges.containsKey(edge.key)) {
+            int edgeIndex = this.edges.get(edge.key);
+            int srcIndex = this.vertices.get(edge.source);
+            int tarIndex = this.vertices.get(edge.target);
+
+            this.incidenceMatrix[srcIndex][edgeIndex] = weight;
+
+            this.incidenceMatrix[tarIndex][edgeIndex] = -weight;
+        }
+    }
 
 
     public void removeVertex(String vertLabel) {
@@ -80,26 +122,38 @@ public class IncidenceMatrix extends AbstractAssocGraph {
 
 
     public void printVertices(PrintWriter os) {
-        // Implement me!
+        for (String vertex : this.vertices.keySet()) {
+            os.print(vertex + " ");
+        }
+        os.println();
     } // end of printVertices()
 
 
     public void printEdges(PrintWriter os) {
-        // Implement me!
+        for (String edge : this.edges.keySet()) {
+            os.print(edge + " ");
+        }
+        os.println();
     } // end of printEdges()
 
     private void addRow() {
         int numRows = this.incidenceMatrix.length + 1;
-        int numCols = (this.incidenceMatrix.length == 0) ? 1 : this.incidenceMatrix[0].length;
+        int numCols = (this.edges.size() == 0) ? 1 : this.incidenceMatrix[0].length;
 
         int[][] newMatrix = new int[numRows][numCols];
+
+        for (int row = 0; row < numRows - 1; row++) {
+            for (int col = 0; col < numCols; col++) {
+                newMatrix[row][col] = this.incidenceMatrix[row][col];
+            }
+        }
 
         this.incidenceMatrix = newMatrix;
     }
 
-    private int[][] addColumn() {
+    private void addColumn() {
         int numRows = this.incidenceMatrix.length;
-        int numCols = this.incidenceMatrix[0].length + 1;
+        int numCols = (this.edges.size() == 0) ? 1 : this.incidenceMatrix[0].length + 1;
 
         int[][] newMatrix = new int[numRows][numCols];
 
@@ -109,29 +163,18 @@ public class IncidenceMatrix extends AbstractAssocGraph {
             }
         }
 
-        return newMatrix;
+        this.incidenceMatrix = newMatrix;
     }
 
-    protected class Edge {
+    private class Edge {
         private String source;
-        private String destination;
-        private int weight;
+        private String target;
+        private String key;
 
-        public Edge(String source, String destination) {
-            this.source = source;
-            this.destination = destination;
-        }
-
-        public String getSource() {
-            return source;
-        }
-
-        public String getDestination() {
-            return destination;
-        }
-
-        public int getWeight() {
-            return weight;
+        private Edge(String source, String target) {
+            this.source = source.toUpperCase();
+            this.target = target.toUpperCase();
+            this.key = this.source + this.target;
         }
     }
 
