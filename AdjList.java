73d4cd30909b368be6ed8863fc0a956f4fd403b6
= <=>
2019-03-31 21:11:06 +1100

    first draft of implementation of AddVertex and Node Class

--- AdjList.java
+++ AdjList.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 /**
  * Adjacency list implementation for the AssociationGraph interface.
- *
+ * <p>
  * Your task is to complete the implementation of this class.  You may add methods, but ensure your modified class compiles and runs.
  *
  * @author Jeffrey Chan, 2019.
  */
 public class AdjList extends AbstractAssocGraph {
 
-    Node head;
 
+    private Vertex[] vertArr = null;
 
     /**
      * Contructs empty graph.
@@ -21,54 +21,97 @@ public class AdjList extends AbstractAssocGraph {
 
     } // end of AdjList()
 
-
     public void addVertex(String vertLabel) {
-        Node node = new Node();
-        node.myPair = new MyPair(vertLabel, 0); // weight is set to 0 since only a vertex is being created with no edges
-        node.next = null;
-
-        if (head == null) {
-            // If head is null it means that the this is the first vertex and therefore it should be the head
-            head = node;
+        if (vertArr == null) {
+            vertArr = new Vertex[1];
+            vertArr[0] = new Vertex(vertLabel);
         } else {
-            // Otherwise create a temporary node to traverse the existing nodes until the last node is reached
-            Node tNode = head;
-            while (tNode.next != null) {
-                tNode = tNode.next;
-            }
-            // Place the new node at the end of the list of existing nodes
-            tNode.next = node;
+            int vertCount = vertArr.length;
+            Vertex[] tempArr = new Vertex[vertCount + 1];
+            for (int i = 0; i < vertCount; i++)
+                tempArr[i] = vertArr[i];
+            tempArr[vertCount] = new Vertex(vertLabel);
+            vertArr = tempArr;
         }
     } // end of addVertex()
 
 
     public void addEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
+        Vertex srcVertex = getVertex(srcLabel);
+
+        if (getVertex(tarLabel) != null)
+            srcVertex.edgeList.addEdge(new MyPair(tarLabel, weight));
     } // end of addEdge()
 
 
     public int getEdgeWeight(String srcLabel, String tarLabel) {
-        // Implement me!
+        Vertex srcVertex = getVertex(srcLabel);
 
-        // update return value
+        try {
+            MyPair myPair = srcVertex.edgeList.getEdge(tarLabel);
+            return myPair.getValue();
+        } catch (GraphAlgorithmsException e) {
+            System.err.println("Graph Algorithm Exception " + e);
+        }
         return EDGE_NOT_EXIST;
     } // end of existEdge()
 
 
     public void updateWeightEdge(String srcLabel, String tarLabel, int weight) {
-        // Implement me!
+        Vertex srcVertex = getVertex(srcLabel);
+
+        try {
+            srcVertex.edgeList.updateEdgeWeight(tarLabel, weight);
+        } catch (GraphAlgorithmsException e) {
+            System.err.println("Graph Algorithm Exception " + e);
+        }
     } // end of updateWeightEdge()
 
 
     public void removeVertex(String vertLabel) {
-        // Implement me!
+        int verInt = getVertIndex(vertLabel);
+        int vertArrLength = vertArr.length;
+
+        //create temp array to fill with all of the remaining elements
+        Vertex[] tempArr = new Vertex[vertArrLength - 1];
+
+        //fill tempArray up to the index of the Vertex being deleted
+        for (int i = 0; i < verInt; i++) {
+            tempArr[i] = vertArr[i];
+        }
+        //resume filling the tempArray with all the elements past the Vertex being deleted
+        for (int i = verInt + 1; i < vertArrLength; i++)
+            tempArr[i] = vertArr[i];
+
+        // set the vertex array to the temp array
+        vertArr = tempArr;
+        vertArrLength = vertArr.length;
+
+        //the loop below removes all the edges which contained the vertex being deleted
+        for (int i = 0; i < vertArrLength; i++) {
+            try {
+                vertArr[i].edgeList.removeEdge(vertLabel);
+            } catch (GraphAlgorithmsException e) {
+            }
+        }
+
     } // end of removeVertex()
 
 
     public List<MyPair> inNearestNeighbours(int k, String vertLabel) {
         List<MyPair> neighbours = new ArrayList<MyPair>();
+        Node head = getVertex(vertLabel).edgeList.head;
 
-        // Implement me!
+        if (head == null)
+            return null;
+
+        Node tNode = head;
+        MyPair tMyPair;
+        while (tNode.next != null) {
+            tMyPair = tNode.myPair;
+            if (tMyPair.getKey().compareTo(vertLabel) == 0 && tMyPair.getValue() > k)
+                neighbours.add(tMyPair);
+        }
 
         return neighbours;
     } // end of inNearestNeighbours()
@@ -76,29 +119,170 @@ public class AdjList extends AbstractAssocGraph {
 
     public List<MyPair> outNearestNeighbours(int k, String vertLabel) {
         List<MyPair> neighbours = new ArrayList<MyPair>();
+        Node head;
+        Node tNode;
+        MyPair tMyPair;
 
-        // Implement me!
+        for (int i = 0; i < vertArr.length; i++) {
+            head = vertArr[i].edgeList.head;
+            if (head != null) {
+                tNode = head;
+                while (tNode.next != null) {
+                    tMyPair = tNode.myPair;
+                    if (tMyPair.getKey().compareTo(vertLabel) == 0 && tMyPair.getValue() > k)
+                        neighbours.add(tMyPair);
+                }
+            }
+        }
 
         return neighbours;
     } // end of outNearestNeighbours()
 
 
     public void printVertices(PrintWriter os) {
-        // Implement me!
+        for(int i=0;i<vertArr.length;i++)
+            os.print(vertArr[i].vertLabel + " ");
     } // end of printVertices()
 
 
     public void printEdges(PrintWriter os) {
-        // Implement me!
+        Node head;
+        Node tNode;
+        MyPair tMyPair;
+
+        for (int i = 0; i < vertArr.length; i++) {
+            head = vertArr[i].edgeList.head;
+            if (head != null) {
+                tNode = head;
+                while (tNode.next != null) {
+                    tMyPair = tNode.myPair;
+                    os.print(vertArr[i].vertLabel + " " + tMyPair.getKey() + " " + tMyPair.getValue());
+                }
+            }
+        }
     } // end of printEdges()
 
+    private Vertex getVertex(String vertLabel) {
 
+        int vertIndex = getVertIndex(vertLabel);
+        if (vertIndex >= 0)
+            return vertArr[vertIndex];
+
+        return null;
+    }
+
+    private int getVertIndex(String vertLabel) {
+        for (int i = 0; i < vertArr.length; i++) {
+            if (vertArr[i].vertLabel.compareTo(vertLabel) == 0)
+                return i;
+        }
+
+        return -1;
+    }
+
+    /**
+     * The Node Class represents nodes for the EdgeList (LinkedList). Each node contains the target vertex and the weight
+     * associated with the source vertex
+     */
     protected class Node {
 
-        MyPair myPair;
-        Node next;
+        private MyPair myPair;
+        private Node next;
+        private Node previous;
 
-        public Node() {
+        public Node(MyPair myPair) {
+            this.myPair = myPair;
         }
+    }
+
+    /**
+     * This class holds the vertex label and corresponding edgeList
+     */
+    protected class Vertex {
+
+        private String vertLabel;
+        private EdgeList edgeList;
+
+        public Vertex(String vertLabel) {
+            this.vertLabel = vertLabel;
+            edgeList = new EdgeList();
+        }
+
+        public String getVertLabel() {
+            return vertLabel;
+        }
+    }
+
+    /**
+     * This class is essentially a LinkedList class but for MyPair types only
+     */
+    protected class EdgeList {
+        Node head;
+        Node tail;
+
+        public void addEdge(MyPair myPair) {
+            Node node = new Node(myPair);
+
+            if (head == null) {
+                // if the head node is nothing it means that this is the first element in the EdgeList therefore both
+                // head and tail are set to the new node
+                head = node;
+                tail = node;
+            } else {
+                // if the head node is not null then this node will be added to the existing list.
+                tail.next = node;
+                node.previous = tail;
+            }
+            // now the new tail is the new node
+            tail = node;
+        }
+
+        public MyPair getEdge(String vertLabel) throws EmptyEdgeListException, EdgeDoesNotExistException {
+            Node node = getNode(vertLabel);
+            return node.myPair;
+        }
+
+        public void updateEdgeWeight(String vertLabel, int weight) throws EmptyEdgeListException, EdgeDoesNotExistException {
+            Node node = getNode(vertLabel);
+            node.myPair = new MyPair(vertLabel, weight);
+        }
+
+        public Node getNode(String vertLabel) throws EmptyEdgeListException, EdgeDoesNotExistException {
+            if (head == null)
+                throw new EmptyEdgeListException();
+
+            Node tNode = head;
+            while (tNode.myPair.getKey().compareTo(vertLabel) != 0) {
+                tNode = tNode.next;
+                if (tNode.next == null) {
+                    throw new EdgeDoesNotExistException();
+                }
+            }
+            return tNode;
+        }
+
+        public void removeEdge(String vertLabel) throws EmptyEdgeListException, EdgeDoesNotExistException {
+            Node node = getNode(vertLabel);
+            Node previousNode = node.previous;
+            Node nextNode = node.next;
+
+            previousNode.next = nextNode;
+            nextNode.previous = previousNode;
+        }
+    }
+
+    protected class GraphAlgorithmsException extends Exception {
+    }
+
+    protected class EmptyEdgeListException extends GraphAlgorithmsException {
+    }
+
+    protected class EdgeDoesNotExistException extends GraphAlgorithmsException {
+    }
+
+
+    //TODO: REMOVE METHODS UNDER HERE:
+    public Vertex[] getVertArr() {
+        return vertArr;
     }
 } // end of class AdjList
